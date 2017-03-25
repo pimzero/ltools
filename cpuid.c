1910535c9c0fc7ad272713c8c77fcd168f83fac9
Pierre MARSAIS <pierre.marsais@lse.epita.fr>
2017-03-25 17:14:05 +0100

    cpuid: handle eax register

--- cpuid.c
+++ cpuid.c
@@ -1,16 +1,40 @@
 #include <unistd.h>
+#include <stdint.h>
+
+void print_hex(const void* val, size_t size)
+{
+	char data[size * 2];
+	static const char chars[] = "0123456789ABCDEF";
+	const char* a = val;
+	for (size_t i = 0; i < sizeof(data); i++) {
+		data[i * 2] = chars[a[i] & 0xf];
+		data[i * 2 + 1] = chars[(a[i] & 0xf0) >> 4];
+	}
+	write(STDOUT_FILENO, "0x", 2);
+	write(STDOUT_FILENO, data, sizeof(data));
+}
 
 int main() {
-	static char name[13];
-	char* address = name;
-	__asm__ __volatile__("mov $0, %%eax\n\t"
+	static union {
+		char c[16];
+		uint32_t i[4];
+	} val;
+	void* address = &val;
+	__asm__ __volatile__("mov %%eax, %%esi\n\t"
+			     "mov $0, %%eax\n\t"
 			     "cpuid\n\t"
-			     "mov %0, %%eax\n\t"
-			     "mov %%ebx, (%%eax)\n\t"
-			     "mov %%edx, 4(%%eax)\n\t"
-			     "mov %%ecx, 8(%%eax)\n\t"
-			     : : "m" (address));
+			     //"mov %0, %%eax\n\t"
+			     "mov %%ebx, (%%esi)\n\t"
+			     "mov %%edx, 4(%%esi)\n\t"
+			     "mov %%ecx, 8(%%esi)\n\t"
+			     "mov %%eax, 12(%%esi)\n\t"
+			     : : "a" (&val));
 
-	name[sizeof(name) - 1] = '\n';
-	write(STDOUT_FILENO, name, sizeof(name));
+	write(STDOUT_FILENO, val.c, 12);
+	write(STDOUT_FILENO, "\n", 1);
+
+	print_hex(&val.i[3], sizeof(*val.i));
+	write(STDOUT_FILENO, "\n", 1);
+
+	return 0;
 }
