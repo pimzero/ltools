c4e043b297e9c063ecfe9d767c9b154e2799c6ad
Pierre Marsais <pierre.marsais@lse.epita.fr>
2017-11-20 00:00:00 +0000

    Add xgetbv

---
+++ xgetbv.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+	uint32_t eax;
+	uint32_t edx;
+	uint32_t ecx = 0;
+
+	char* noargs[] = {
+		"0",
+		NULL,
+	};
+
+	if (argc == 1)
+		argv = noargs;
+	else
+		argv++;
+
+	do {
+		ecx = atoi(*argv);
+		argv++;
+		__asm__ __volatile__("xgetbv\n\t"
+				: "=a"(eax), "=d"(edx) : "c"(ecx));
+
+		printf("ecx: 0x%x, eax: 0x%x, edx: 0x%x\n", ecx, eax, edx);
+	} while (*argv);
+
+	return 0;
+}
