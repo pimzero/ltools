7fcd89fd365ca434346402612cb3f872f12c2fb7
Pierre Marsais <pmarsais@google.com>
2022-08-16 00:54:47 +0100

    writeio: Minor cleanups

--- writeio.c
+++ writeio.c
@@ -3,10 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-
 
 int main(int argc, char** argv) {
 	(void) argc;
