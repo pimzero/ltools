1f52ef8e3e748fa59008dc6ba58a743e57ca4758
Pierre Marsais <pmarsais@google.com>
2022-08-16 00:55:47 +0100

    smsw: Minor cleanups

--- smsw.c
+++ smsw.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 __asm__(".global smsw\n"
 "smsw:\n\t"
@@ -8,8 +8,7 @@ __asm__(".global smsw\n"
 
 extern uint16_t smsw(void);
 
-static void print_cr0(uint16_t cr0)
-{
+static void print_cr0(uint16_t cr0) {
 	printf("CR0:");
 #define X(Bit, Name) do { if (cr0 & (1 << (Bit))) printf(" " #Name); } while (0)
 	X(0, PE);
@@ -22,7 +21,6 @@ static void print_cr0(uint16_t cr0)
 	printf("\n");
 }
 
-int main()
-{
+int main() {
 	print_cr0(smsw());
 }
